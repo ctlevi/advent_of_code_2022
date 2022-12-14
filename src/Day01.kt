cd070e476477bfc995712866749b4d265494f950
Chris Tate <chris@youneedabudget.com>
2022-12-14 12:38:26 -0500

    Solution for Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var currentElfCalorieCount: Int = 0
+        var highestCalorieCount: Int = 0
+        for (row in input) {
+            if (row == "") {
+                if (currentElfCalorieCount > highestCalorieCount) {
+                    highestCalorieCount = currentElfCalorieCount
+                }
+                currentElfCalorieCount = 0
+            } else {
+                currentElfCalorieCount += row.toInt()
+            }
+        }
+        return highestCalorieCount;
+    }
+
+    fun tryReplaceLowestCount(toCheck: Int, counts: MutableList<Int>) {
+        if (toCheck > counts.last()) {
+            counts.set(counts.size - 1, toCheck)
+            counts.sort()
+            counts.reverse()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var currentElfCalorieCount: Int = 0
+        var threeHighestCalorieCounts: MutableList<Int> = mutableListOf(0, 0, 0)
+        for (row in input) {
+            if (row == "") {
+                tryReplaceLowestCount(currentElfCalorieCount, threeHighestCalorieCounts)
+                currentElfCalorieCount = 0
+            } else {
+                currentElfCalorieCount += row.toInt()
+            }
+        }
+        return threeHighestCalorieCounts.sum();
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testResult = part1(testInput)
+    "Test Result: ${testResult}".println()
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    "Part 1 Result: ${part1(input)}".println()
+    "Part 2 Result: ${part2(input)}".println()
 }
